2af32d743f6a901efc7117920559284dcf1f1664
shunak <shuna966@gmail.com>
2021-10-31 23:58:40 +0900

    wip

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,4 +1,5 @@
 extern crate add_one;
+extern crate rand;
 
 fn main() {
     let num = 10;
---
+++ target/debug/deps/adder-61d3331c03d1e74a.d
@@ -0,0 +1,5 @@
+/root/add/target/debug/deps/adder-61d3331c03d1e74a: adder/src/main.rs
+
+/root/add/target/debug/deps/adder-61d3331c03d1e74a.d: adder/src/main.rs
+
+adder/src/main.rs:
